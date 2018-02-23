e0882f58bbbdb705c9c3f7cbd72398596fab13fd
EmilioOldenziel <emilio.oldenziel@gmail.com>
2018-02-23 14:31:14 +0100

    cleanup

--- SciVisQT/glwidget.cpp
+++ SciVisQT/glwidget.cpp
@@ -67,10 +67,12 @@ int GLWidget::clamp(float x)
 void GLWidget::rainbow(float value,float* R,float* G,float* B)
 {
     const float dx=0.8;
-    if (value<0)
+    if (value<0){
         value=0;
-    if (value>1)
+    }
+    if (value>1){
         value=1;
+    }
     value = (6-2*dx)*value+dx;
     *R = value;//max(0.0,(3-fabs(value-4)-fabs(value-5))/2);
     *G = value;//max(0.0,(4-fabs(value-2)-fabs(value-4))/2);
@@ -110,7 +112,6 @@ void GLWidget::set_colormap(float vy)
     vy = colorBands(vy, bands);
     float R,G,B;
     if (scalar_col==COLOR_BLACKWHITE){
-
         R = G = B = vy;
     }
     else if (scalar_col==COLOR_RAINBOW)
--- SciVisQT/glwidget.h
+++ SciVisQT/glwidget.h
@@ -55,17 +55,19 @@ private:
     void setupVertexAttribs();
 
     int   windowWidth, windowHeight;      //size of the graphics window, in pixels
-    int DIM;
+    int   DIM;
     int   color_dir = 0;            //use direction color-coding or not
-    float vec_scale = 10;			//scaling of hedgehogs
-    int   draw_smoke = 1;           //draw the smoke or not
+    float vec_scale = 1;			//scaling of hedgehogs
+    int   draw_smoke = 0;           //draw the smoke or not
     int   draw_vecs = 1;            //draw the vector field or not
+
     const int COLOR_BLACKWHITE=0;   //different types of color mapping: black-and-white, rainbow, banded
     const int COLOR_RAINBOW=1;
     const int COLOR_HEAT=2;
     const int COLOR_YELLOW = 3;
     int   bands = 256;
-    int   scalar_col = 2;           //method for scalar coloring
+
+    int   scalar_col = 1;           //method for scalar coloring
     int   frozen = 0;               //toggles on/off the animation
 
     Simulation simulation;
